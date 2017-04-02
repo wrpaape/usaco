0bb352ddf603cc150f869a8abe74c97e0e8b87fb
Reid Paape <wrpaape@gmail.com>
2017-04-02 11:44:05 -0500

    wormhole with links implemented as indices instead of pointers

--- sec1/sub3/wormhole/wormhole.c
+++ sec1/sub3/wormhole/wormhole.c
@@ -9,177 +9,79 @@ TASK: wormhole
 #define N_MAX 12
 
 
-struct YNode {
-	size_t y;
-	size_t count;
-	struct YNode *restrict next;
-};
+static int N;          /* count of wormholes */
+static int loop_count; /* solution */
 
-static struct YNode *restrict y_map[N_MAX];
-static struct YNode y_nodes[N_MAX];
-static struct YNode *restrict y_node      = &y_nodes[0];
-static struct YNode *restrict y_nodes_end = &y_nodes[0];
+/* wormholes are recorded by integer id in the range [1, N] */
 
-struct GraphNode {
-	unsigned int visited;
-	struct GraphNode *jump;
-	struct GraphNode *forward;
-	struct GraphNode *next;
-};
+/* jump_to[worm1] = worm2, jump_to[worm2] = worm1 */
+static int jump_to[N_MAX + 1];
 
-static struct GraphNode graph_nodes[N_MAX];
-static struct GraphNode *restrict graph_nodes_begin = &graph_nodes[0];
-static struct GraphNode *restrict graph_nodes_end   = &graph_nodes[0];
-static struct GraphNode *restrict rem_unpaired;
+/* walk_to[worm1] = worm2 (worm2 == 0 if end of row) */
+static int walk_to[N_MAX + 1];
 
-static size_t N;
-static size_t loop_count;
+/* y_coord[worm] = y */
+static int y_coord[N_MAX + 1];
 
-static inline void
-update_map(const size_t y)
-{
-	struct YNode *restrict *restrict bucket;
-	struct YNode *restrict y_node;
+/* is_paired[worm] = 1 if is paired, 0 if available */
+static int is_paired[N_MAX + 1];
 
-
-	bucket = &y_map[y % N];
-
-	while (y_node = *bucket, y_node) {
-		if (y_node->y == y) {
-			++(y_node->count);
-			return;
-		}
-
-		bucket = &y_node->next;
-	}
-
-	y_nodes_end->y     = y;
-	y_nodes_end->count = 1;
-
-	*bucket = y_nodes_end++;
-}
-
-static inline void
-clear_visted(void)
-{
-	struct GraphNode *restrict graph_node;
-	graph_node = graph_nodes_begin;
-	do {
-		graph_node->visited = 0;
-	} while (++graph_node < graph_nodes_end);
-}
 
 static inline void
 check_loop(void)
 {
-	struct GraphNode *restrict graph_node;
-	struct GraphNode *restrict start_node;
+	int start_worm, worm, step;
 
-	start_node = graph_nodes_begin;
-	do {
-		clear_visted();
+	for (start_worm = 1; start_worm <= N; ++start_worm) {
+		worm = start_worm;
 
-		graph_node = start_node;
+		for (step = 1; step <= N; ++step)
+			worm = walk_to[jump_to[worm]]; /* jump -> walk +x */
 
-		while (1) {
-			/* mark nodes that have been jumped */
-			graph_node->visited = 1;
-
-			graph_node = graph_node->jump->forward;
-
-			if (!graph_node)
-				break;
-
-			if (graph_node->visited) {
-				++loop_count;
-				return;
-			}
-		}
-	} while (++start_node < graph_nodes_end);
-}
-
-static inline void
-init_row(size_t count)
-{
-	struct GraphNode *restrict next_graph_node;
-	struct GraphNode *restrict prev_graph_node;
-
-	prev_graph_node = graph_nodes_end++;
-
-	while (1) {
-		prev_graph_node->next = rem_unpaired;
-		rem_unpaired          = prev_graph_node;
-
-		if (--count == 0)
+		if (worm != 0) {
+			/* if didn't get stuck at NULL worm
+			 * do to walking off of a row, loop
+			 * if found -> increment solution */
+			++loop_count;
 			return;
-
-		next_graph_node          = graph_nodes_end++;
-		prev_graph_node->forward = next_graph_node;
-		prev_graph_node          = next_graph_node;
+		}
 	}
 }
 
-void
-comb_pairs(void)
-{
-	struct GraphNode *restrict node1;
-	struct GraphNode *restrict node2;
-	struct GraphNode *restrict *restrict prev_ptr;
-	struct GraphNode *restrict *restrict node1_jump;
-	struct GraphNode *restrict next;
 
-	if (!rem_unpaired) {
-		check_loop(); /* all nodes paired, check cycle */
+void
+solve(int rem_pairs)
+{
+	int worm1, worm2;
+
+	if (rem_pairs == 0) {
+		check_loop(); /* no more pairing */
 		return;
 	}
 
-	/* pop 'node1' from head of 'rem_unpaired'
-	 * traverse remaining list, popping 'node2'
-	 * from middle and pairing with 'node1'
-	 */
+	--rem_pairs;
 
-	node1        = rem_unpaired;
-	node1_jump   = &node1->jump;
-	rem_unpaired = rem_unpaired->next;
-	prev_ptr     = &rem_unpaired;
-	node2        = rem_unpaired;
+	for (worm1 = 1; is_paired[worm1]; ++worm1)
+		; /* set worm1 to 1st unpaired */
 
-	while (1) {
-		/* pair nodes */
-		*node1_jump = node2;
-		node2->jump = node1;
+	is_paired[worm1] = 1; /* mark worm1 */
 
-		/* pop node2 from list */
-		next      = node2->next;
-		*prev_ptr = next;
+	for (worm2 = worm1 + 1; worm2 <= N; ++worm2) {
+		if (is_paired[worm2])
+			continue;
 
-		/* recurse */
-		comb_pairs();
+		is_paired[worm2] = 1; /* mark worm2 */
 
-		/* re-insert node2 */
-		*prev_ptr = node2;
+		/* pair worm1 with worm2 */
+		jump_to[worm1] = worm2;
+		jump_to[worm2] = worm1;
 
-		if (!next)
-			break; /* none left */
+		solve(rem_pairs);
 
-		prev_ptr = &node2->next;
-		node2    = next;
+		is_paired[worm2] = 0; /* unmark worm2 */
 	}
 
-	rem_unpaired = node1; /* restore rem_unpaired */
-}
-
-
-static inline void
-solve(void)
-{
-	/* link nodes on same row (same y) by 'forward' */
-	do {
-		init_row(y_node->count);
-	} while (++y_node < y_nodes_end);
-
-	/* check each possible pairing */
-	comb_pairs();
+	is_paired[worm1] = 0; /* unmark worm1 */
 }
 
 
@@ -188,25 +90,35 @@ main(void)
 {
 	FILE *input;
 	FILE *output;
-	size_t x;
-	size_t y;
-	size_t i;
+	int x; /* ignored */
+	int y1, y2;
+	int worm, worm1, worm2;
 
 	assert(input = fopen("wormhole.in", "r"));
-	assert(fscanf(input, "%zu\n", &N) == 1);
+	assert(fscanf(input, "%d\n", &N) == 1);
 
-	i = N;
-	do {
-		assert(fscanf(input, "%zu %zu\n", &x, &y) == 2);
-		update_map(y);
-	} while (--i > 0);
+	for (worm = 1; worm <= N; ++worm)
+		assert(fscanf(input, "%d %d\n", &x, &y_coord[worm]) == 2);
 
 	assert(fclose(input) == 0);
 
-	solve();
+	for (worm1 = 1; worm1 <= N; ++worm1) {
+		y1 = y_coord[worm1];
+
+		for (worm2 = worm1 + 1; worm2 <= N; ++worm2) {
+			y2 = y_coord[worm2];
+
+			if (y1 == y2) {
+				walk_to[worm1] = worm2; /* same row -> link */
+				break;
+			}
+		}
+	}
+
+	solve(N / 2); /* N / 2 available pairs */
 
 	assert(output = fopen("wormhole.out", "w"));
-	assert(fprintf(output, "%zu\n", loop_count) > 0);
+	assert(fprintf(output, "%d\n", loop_count) > 0);
 	assert(fclose(output) == 0);
 	return 0;
 }
