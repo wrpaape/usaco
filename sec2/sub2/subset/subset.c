e2b74059731bf52ebf599bb6c004217d7158f607
Reid Paape <wrpaape@gmail.com>
2017-04-15 16:03:32 -0500

    stupid 32 bit longs, subset solved

--- sec2/sub2/subset/subset.c
+++ sec2/sub2/subset/subset.c
@@ -7,80 +7,82 @@ TASK: subset
 #include <stdio.h>
 #include <assert.h>
 
-#define N_MAX 39
+#define N_MAX		39
+#define TARGET_MAX	((N_MAX * (N_MAX + 1)) / 4)
 
-static unsigned int N;
-static unsigned int sum_half;
 
-unsigned int
-do_partition(unsigned int acc_sum,
-	     unsigned int pick,
-	     unsigned int pick_upto)
+static long long cache[N_MAX + 1][TARGET_MAX + 1];
+
+long long
+count_combinations(const long long n_max,
+		   const long long target);
+
+static inline long long
+solve_combinations(const long long n_max,
+		   const long long target)
 {
-	unsigned int next_sum, count_even;
+	long long n_max_m1;
 
-	if (pick_upto > N)
-		return (acc_sum == sum_half);
+	if (n_max >= target) /* ignore range [target + 1, n_max] */
+		return 1 + count_combinations(target - 1, target);
 
-	count_even = 0;
-	++pick_upto;
-	do {
-		next_sum = acc_sum + pick;
+	n_max_m1 = n_max - 1;
 
-		if (next_sum > sum_half)
-			break;
+	return count_combinations(n_max_m1, target - n_max)
+	     + count_combinations(n_max_m1, target);
+}
 
-		++pick;
+long long
+count_combinations(const long long n_max,
+		   const long long target)
+{
+	long long *restrict cache_ptr;
+	long long cached_solution;
 
-		count_even += do_partition(next_sum,
-					   pick,
-					   pick_upto);
-	} while (pick < pick_upto);
+	cache_ptr       = &cache[n_max][target];
+	cached_solution = *cache_ptr;
 
-	return count_even;
+	if (cached_solution < 0) {
+		cached_solution = solve_combinations(n_max, target);
+		*cache_ptr      = cached_solution;
+	}
+
+	return cached_solution;
+}
+
+static inline long long
+solve(const long long sum_1_to_N_div_2,
+      const long long N)
+{
+	long long n, target;
+	for (n = 1; n <= N; ++n)
+		for (target = 1; target <= sum_1_to_N_div_2; ++target)
+			cache[n][target] = -1;
+
+	/* leave cache[0...N][0] and cache[0][0...target] == 0 */
+
+	return count_combinations(N, sum_1_to_N_div_2) / 2;
 }
 
 int
 main(void)
 {
 	FILE *input, *output;
-	unsigned int n_p1, n_n_p1;
-	unsigned int rem, group_size, max_group_size;
-	unsigned int total_count_even, last_count_even;
+	long long N, n_n_p1, rem, total_count;
 
 	assert(input = fopen("subset.in", "r"));
-	assert(fscanf(input, "%u\n", &N) == 1);
+	assert(fscanf(input, "%lld\n", &N) == 1);
 	assert(fclose(input) == 0);
 
-	n_p1     = N + 1;
-	n_n_p1   = N * n_p1;
+	n_n_p1   = N * (N + 1);
 	rem      = n_n_p1 % 4;
 
-	total_count_even = 0;
-
-	if (!rem) {
-		/* sum of 1 ... N can be divided evenly in 2 */
-		sum_half = n_n_p1 / 4;
-
-		max_group_size = (N / 2);
-
-		for (group_size = 1; group_size < max_group_size; ++group_size)
-			total_count_even += do_partition(0, 1, n_p1 - group_size);
-
-		last_count_even = do_partition(0, 1, n_p1 - max_group_size);
-
-
-		/* if N is even, last paritition will return a double count
-		 * of possible even splits */
-		if ((N & 1) == 0)
-			last_count_even /= 2;
-
-		total_count_even += last_count_even;
-
-	}
+	/* if sum of 1 ... N cannot be divided evenly in 2 -> 0 possible
+	 * partions, else call 'solve' */
+	total_count = rem ? 0 : solve(n_n_p1 / 4, N);
 
 	assert(output = fopen("subset.out", "w"));
-	assert(fprintf(output, "%u\n", total_count_even) > 0);
+	assert(fprintf(output, "%lld\n", total_count) > 0);
 	assert(fclose(output) == 0);
 	return 0;
 }
---
+++ sec2/sub2/subset/subset.old.c
@@ -0,0 +1,87 @@
+/*
+ID: wrpaape1
+LANG: C
+TASK: subset
+*/
+
+#include <stdio.h>
+#include <assert.h>
+
+#define N_MAX 39
+
+static unsigned int N;
+static unsigned int sum_half;
+
+unsigned int
+do_partition(unsigned int acc_sum,
+	     unsigned int pick,
+	     unsigned int pick_upto)
+{
+	unsigned int next_sum, count_even;
+
+	if (pick_upto > N)
+		return (acc_sum == sum_half);
+
+	++pick_upto;
+	count_even = 0;
+
+	do {
+		next_sum = acc_sum + pick;
+
+		if (next_sum > sum_half)
+			break;
+
+		++pick;
+
+		count_even += do_partition(next_sum,
+					   pick,
+					   pick_upto);
+	} while (pick < pick_upto);
+
+	return count_even;
+}
+
+int
+main(void)
+{
+	FILE *input, *output;
+	unsigned int n_p1, n_n_p1;
+	unsigned int rem, group_size, max_group_size;
+	unsigned int total_count_even, last_count_even;
+
+	assert(input = fopen("subset.in", "r"));
+	assert(fscanf(input, "%u\n", &N) == 1);
+	assert(fclose(input) == 0);
+
+	n_p1     = N + 1;
+	n_n_p1   = N * n_p1;
+	rem      = n_n_p1 % 4;
+
+	total_count_even = 0;
+
+	if (!rem) {
+		/* sum of 1 ... N can be divided evenly in 2 */
+		sum_half = n_n_p1 / 4;
+
+		max_group_size = (N / 2);
+
+		for (group_size = 1; group_size < max_group_size; ++group_size)
+			total_count_even += do_partition(0, 1, n_p1 - group_size);
+
+		last_count_even = do_partition(0, 1, n_p1 - max_group_size);
+
+
+		/* if N is even, last paritition will return a double count
+		 * of possible even splits */
+		if ((N & 1) == 0)
+			last_count_even /= 2;
+
+		total_count_even += last_count_even;
+
+	}
+
+	assert(output = fopen("subset.out", "w"));
+	assert(fprintf(output, "%u\n", total_count_even) > 0);
+	assert(fclose(output) == 0);
+	return 0;
+}
