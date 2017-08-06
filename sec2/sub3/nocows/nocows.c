2a7aaed150a3ac5025b0fb4dc23f0f43f299c442
wrpaape <wrpaape@gmail.com>
2017-08-06 18:07:35 -0500

    fleshed out basic draft of nocows

--- sec2/sub3/nocows/nocows.c
+++ sec2/sub3/nocows/nocows.c
@@ -7,18 +7,72 @@ TASK: nocows
 #include <stdio.h>
 #include <assert.h>
 
+#define N_MAX 200
+#define K_MAX 100
 
-static inline size_t
-solve(unsigned int rem_cows,
-      unsigned int rem_height)
+
+static unsigned int lookup[K_MAX + 1][N_MAX + 1];
+
+static unsigned int
+solve(const unsigned int rem_height,
+      const unsigned int rem_cows);
+
+
+static inline unsigned int
+do_solve(unsigned int rem_height,
+	 unsigned int rem_cows)
 {
+	if (rem_cows == 0)
+		return 1; // successfully placed all cows
+
 	if (rem_height == 0)
-		return (rem_cows == 0);
+		return 0; // out of height, cows remain
 
-	size_t total_peds;
+	--rem_height; // reduce remaining tree height
+	--rem_cows;   // place 1 root cow
 
-	--rem_cows;
+	unsigned int odd_peds;
 
+	unsigned int total_peds     = 0;
+	unsigned int rem_cows_left  = 0;
+	unsigned int rem_cows_right = rem_cows;
+
+
+	while (1) {
+		odd_peds = solve(rem_height,
+				 rem_cows_left);
+
+		if (rem_cows_left == rem_cows_right)
+			return (total_peds * 2) + odd_peds;
+
+		++rem_cows_left;
+
+		total_peds += odd_peds;
+		total_peds += solve(rem_height,
+				    rem_cows_right);
+
+		if (rem_cows_left == rem_cows_right)
+			return total_peds * 2;
+
+		--rem_cows_right;
+	}
+}
+
+static unsigned int
+solve(unsigned int rem_height,
+      unsigned int rem_cows)
+{
+
+	unsigned int *const cache_ptr = &lookup[rem_height][rem_cows];
+	unsigned int cached    	      = *cache_ptr;
+
+	if (cached == 0) {
+		cached = do_solve(rem_height,
+				  rem_cows);
+		*cache_ptr = cached;
+	}
+
+	return cached;
 }
 
 
@@ -32,10 +86,12 @@ main(void)
 	assert(fscanf(input, "%u %u\n", &N, &K) == 2);
 	assert(fclose(input) == 0);
 
-	const size_t total_peds = solve(N, K);
+	const unsigned int total_peds = solve(K, N);
+
+	assert(total_peds >= 0);
 
 	assert(output = fopen("nocows.out", "w"));
-	assert(fprintf(output, "%zu\n", total_peds % 9901) > 0);
+	assert(fprintf(output, "%u\n", total_peds % 9901) > 0);
 	assert(fclose(output) == 0);
 
 	return 0;
