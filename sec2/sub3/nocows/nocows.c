0d24d1e9b7ca09ad8be31852dfabd1a99f03809f
wrpaape <wrpaape@gmail.com>
2017-08-13 21:51:52 -0500

    complete/incomplete solutions at every height/cows combination taken into consideration...

--- sec2/sub3/nocows/nocows.c
+++ sec2/sub3/nocows/nocows.c
@@ -20,9 +20,9 @@ struct Solution
 	short incomplete;
 };
 
-static Solution lookup[K_MAX + 1][N_MAX + 1];
+static struct Solution lookup[K_MAX + 1][N_MAX + 1];
 
-static struct Solution *
+static const struct Solution *
 solve(const unsigned int rem_height,
       const unsigned int rem_cows);
 
@@ -32,44 +32,85 @@ do_solve(unsigned int rem_height,
 	 unsigned int rem_cows,
 	 struct Solution *const solution)
 {
-	--rem_cows; // place 1 root cow
-	if (rem_cows <= 1)
-		return (rem_cows == 0); // placed all cows OR 1 extra child
-
+	--rem_cows;   // place 1 root cow
 	--rem_height; // reduce remaining tree height
-	if (rem_height == 0)
-		return 0; // out of height, cows remain (no solution)
 
-	unsigned int peds_left;
-	unsigned int peds_right;
-	unsigned int total_peds = 0;
-	unsigned int rem_cows_left  = 1;
-	unsigned int rem_cows_right = rem_cows - 1;
+	const unsigned int out_of_height = (rem_height == 0);
+
+	if (rem_cows <= 1) {
+		// placed all cows OR 1 extra child
+		const unsigned int out_of_cows = (rem_cows == 0);
+		solution->complete   = out_of_cows & out_of_height;
+		solution->incomplete = out_of_cows & !out_of_height;
+		return;
+	}
+
+	if (out_of_height) {
+		// out of height, cows remain (no solution)
+		solution->complete   = 0;
+		solution->incomplete = 0;
+		return;
+	}
+
+	const struct Solution *peds_left;
+	const struct Solution *peds_right;
+	unsigned int complete_left;
+	unsigned int incomplete_left;
+	unsigned int complete_right;
+	unsigned int incomplete_right;
+
+	unsigned int total_complete   = 0;
+	unsigned int total_incomplete = 0;
+	unsigned int rem_cows_left    = 1;
+	unsigned int rem_cows_right   = rem_cows - 1;
 
 	while (1) {
-		peds_left = solve(rem_height,
-				  rem_cows_left);
+		peds_left 	= solve(rem_height,
+				       	rem_cows_left);
+		complete_left   = peds_left->complete;
+		incomplete_left = peds_left->incomplete;
 
 		if (rem_cows_left == rem_cows_right) {
-			total_peds += ((peds_left * peds_left) % 9901);
-			total_peds %= 9901;
-			return total_peds;
+			total_complete += ((complete_left * complete_left) % 9901);
+			total_complete %= 9901;
+
+			total_complete += ((incomplete_left * complete_left) % 9901);
+			total_complete %= 9901;
+
+			total_incomplete += ((incomplete_left * incomplete_left) % 9901);
+			total_incomplete %= 9901;
+			break;
 		}
 
-		peds_right = solve(rem_height,
-				   rem_cows_right);
+		peds_right 	 = solve(rem_height,
+					 rem_cows_right);
+		complete_right   = peds_right->complete;
+		incomplete_right = peds_right->incomplete;
 
-		total_peds += ((peds_left * peds_right * 2) % 9901);
-		total_peds %= 9901;
+		total_complete += ((complete_left * complete_right * 2) % 9901);
+		total_complete %= 9901;
+
+		total_complete += ((incomplete_left * complete_right * 2) % 9901);
+		total_complete %= 9901;
+
+		total_complete += ((complete_left * incomplete_right * 2) % 9901);
+		total_complete %= 9901;
+
+		total_incomplete += ((incomplete_left * incomplete_right * 2) % 9901);
+		total_incomplete %= 9901;
 
 		++rem_cows_left;
 
 		if (rem_cows_left == rem_cows_right)
-			return total_peds;
+			break;
 
 		--rem_cows_right;
 	}
+
+	solution->complete   = total_complete;
+	solution->incomplete = total_incomplete;
 }
+
 
 static const struct Solution *
 solve(unsigned int rem_height,
@@ -99,7 +140,7 @@ main(void)
 	assert(fclose(input) == 0);
 
 	// set those below [N|K]_MIN for trivial cases
-	const size_t size_of_n_range = sizeof(int) * (N + 1);
+	const size_t size_of_n_range = sizeof(lookup[0][0]) * (N + 1);
 
 	for (unsigned int k = 0; k <= K; ++k)
 		(void) memset(&lookup[k][0],
