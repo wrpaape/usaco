562f817ef43d2ca39e25ca54edf80ae3f789b929
William Reid Paape <wrpaape@gmail.com>
2019-10-25 15:56:02 -0500

    nocows

--- sec2/sub3/nocows/nocows.c
+++ sec2/sub3/nocows/nocows.c
@@ -6,132 +6,68 @@ TASK: nocows
 
 #include <stdio.h>
 #include <assert.h>
-#include <string.h>
 
-#define N_MIN 3
-#define N_MAX 199
-#define K_MIN 2
-#define K_MAX 99
-
-
-struct Solution
+unsigned int
+solve(unsigned int n,
+      unsigned int k)
 {
-	short complete;
-	short incomplete;
-};
+    enum {
+        N_MAX = 200,
+        K_MAX = 100
+    };
+    static const unsigned int MAGIC_NUMBER = 9901;
 
-static struct Solution lookup[K_MAX + 1][N_MAX + 1];
+    unsigned short SOLUTION[K_MAX + 1][N_MAX + 1];
+    SOLUTION[1][1] = 1;
 
-static const struct Solution *
-solve(const unsigned int rem_height,
-      const unsigned int rem_cows);
+    unsigned int max_child_nodes = n - 2;
+    for (unsigned int child_height1 = 1; child_height1 < k; ++child_height1) {
+        unsigned int parent_height = child_height1 + 1;
+        for (unsigned int child_height2 = 1; child_height2 <= child_height1;
+             ++child_height2) {
+            for (unsigned int child_nodes1 = 1; child_nodes1 <= max_child_nodes;
+                 child_nodes1 += 2) {
+                for (unsigned int child_nodes2 = 1;
+                     child_nodes2 <= child_nodes1; child_nodes2 += 2) {
+                    unsigned int combs = SOLUTION[child_height1][child_nodes1]
+                                       * SOLUTION[child_height2][child_nodes2];
+                    combs %= MAGIC_NUMBER;
 
+                    if (child_nodes1 != child_nodes2) {
+                        unsigned int combs2 =
+                            SOLUTION[child_height1][child_nodes2]
+                          * SOLUTION[child_height2][child_nodes1];
+                        combs2 %= MAGIC_NUMBER;
 
-static inline void
-do_solve(unsigned int rem_height,
-	 unsigned int rem_cows,
-	 struct Solution *const solution)
-{
-	--rem_cows;   // place 1 root cow
-	--rem_height; // reduce remaining tree height
+                        combs += combs2;
+                        combs %= MAGIC_NUMBER;
+                    }
 
-	const unsigned int out_of_height = (rem_height == 0);
+                    if (child_height1 != child_height2) {
+                        combs *= 2;
+                        combs %= MAGIC_NUMBER;
+                    }
 
-	if (rem_cows <= 1) {
-		// placed all cows OR 1 extra child
-		const unsigned int out_of_cows = (rem_cows == 0);
-		solution->complete   = out_of_cows & out_of_height;
-		solution->incomplete = out_of_cows & !out_of_height;
-		return;
-	}
+                    unsigned int parent_nodes = child_nodes1
+                                              + child_nodes2
+                                              + 1;
+                    unsigned short *parent_combs_ptr =
+                        &SOLUTION[parent_height][parent_nodes];
 
-	if (out_of_height) {
-		// out of height, cows remain (no solution)
-		solution->complete   = 0;
-		solution->incomplete = 0;
-		return;
-	}
+                    unsigned int parent_combs = *parent_combs_ptr;
 
-	const struct Solution *peds_left;
-	const struct Solution *peds_right;
-	unsigned int complete_left;
-	unsigned int incomplete_left;
-	unsigned int total_left;
-	unsigned int complete_right;
-	unsigned int incomplete_right;
-	unsigned int total_right;
-	unsigned int count_combined;
+                    /* increment the parent combinations */
+                    parent_combs += combs;
+                    parent_combs %= MAGIC_NUMBER;
 
-	unsigned int total_complete   = 0;
-	unsigned int total_incomplete = 0;
-	unsigned int rem_cows_left    = 1;
-	unsigned int rem_cows_right   = rem_cows - 1;
+                    *parent_combs_ptr = (unsigned short) parent_combs;
+                }
+            }
+        }
+    }
 
-	while (1) {
-		peds_left	= solve(rem_height,
-					rem_cows_left);
-		complete_left   = peds_left->complete;
-		incomplete_left = peds_left->incomplete;
-		total_left      = (complete_left + incomplete_left) % 9901;
-
-		if (rem_cows_left == rem_cows_right) {
-			count_combined  = complete_left * total_left;
-			total_complete += (count_combined % 9901);
-			total_complete %= 9901;
-
-			count_combined    = incomplete_left * incomplete_left;
-			total_incomplete += (count_combined % 9901);
-			total_incomplete %= 9901;
-			break;
-		}
-
-		peds_right	 = solve(rem_height,
-					 rem_cows_right);
-		complete_right   = peds_right->complete;
-		incomplete_right = peds_right->incomplete;
-		total_right      = complete_right + incomplete_right;
-
-		count_combined  = complete_right * total_left * 2;
-		total_complete += (count_combined % 9901);
-		total_complete %= 9901;
-
-		count_combined  = complete_left * total_right * 2;
-		total_complete += (count_combined % 9901);
-		total_complete %= 9901;
-
-		count_combined    = incomplete_left * incomplete_right * 2;
-		total_incomplete += (count_combined % 9901);
-		total_incomplete %= 9901;
-
-		++rem_cows_left;
-
-		if (rem_cows_left == rem_cows_right)
-			break;
-
-		--rem_cows_right;
-	}
-
-	solution->complete   = total_complete;
-	solution->incomplete = total_incomplete;
+    return SOLUTION[k][n];
 }
-
-
-static const struct Solution *
-solve(unsigned int rem_height,
-      unsigned int rem_cows)
-{
-
-	struct Solution *const solution = &lookup[rem_height][rem_cows];
-
-	if (solution->complete < 0) {
-		do_solve(rem_height,
-			 rem_cows,
-			 solution);
-	}
-
-	return solution;
-}
-
 
 int
 main(void)
@@ -143,18 +79,10 @@ main(void)
 	assert(fscanf(input, "%u %u\n", &N, &K) == 2);
 	assert(fclose(input) == 0);
 
-	// set those below [N|K]_MIN for trivial cases
-	const size_t size_of_n_range = sizeof(lookup[0][0]) * (N + 1);
-
-	for (unsigned int k = 0; k <= K; ++k)
-		(void) memset(&lookup[k][0],
-			      -1, // safe, 2s compliment all 1's
-			      size_of_n_range);
-
-	const struct Solution *const solution = solve(K, N);
+	unsigned int solution = solve(N, K);
 
 	assert(output = fopen("nocows.out", "w"));
-	assert(fprintf(output, "%hd\n", solution->complete) > 0);
+	assert(fprintf(output, "%u\n", solution) > 0);
 	assert(fclose(output) == 0);
 
 	return 0;
